7d6592d0d12897f73acb429e40b8efc7cf17d203
Anton Osten <anton@ostensible.me>
2015-08-07 22:02:31 -0500

    pick best set and the unique segment

--- natclasses.py
+++ natclasses.py
@@ -6,64 +6,152 @@ from itertools import zip_longest, chain
 import json
 from pprint import pprint
 
-def find_letters(word_list):
-    words = set(word_list)
+def ngramicise(word_list, n=2):
+    for word in word_list:
+        current = 0
+        next = current + n
+        ngram_word = []
+        while next < len(word) + 1:
+            ngram_word.append(tuple(word[current:next]))
+            current += 1
+            next = current + n
+        yield ngram_word
 
-    for segments in zip_longest(*words):
+
+def find_letters(word_list, reverse=False):
+
+    for segments in zip_longest(*word_list):
         counted_segments = Counter(segments)
         yield counted_segments
 
 
 def get_differences(first_set, second_set):
 
+    unique_set1 = []
+    unique_set2 = []
     for letters1, letters2 in zip_longest(first_set, second_set, fillvalue=set()):
         first_letters, second_letters = set(letters1), set(letters2)
-        difference = first_letters.difference(second_letters)
-        if difference in first_letters:
-            group = 1
-        else:
-            group = 2
-        if difference:
-            yield difference, group
 
-def filter_noise(segment_list):
-    for counted_segments in segment_list:
-        num_all_segments = sum(counted_segments.values())
-        threshold = min(num_all_segments * 0.001, 5)
-        good_segments = {segment for segment, count in counted_segments.items()
-                         if count >= threshold}
+        unique_1 = first_letters.difference(second_letters)
+        unique_2 = second_letters.difference(first_letters)
+
+        unique_set1.append(unique_1)
+        unique_set2.append(unique_2)
+
+    return unique_set1, unique_set2
+
+
+def pick_best_segment(scores):
+    maximum = max(enumerate(scores), key=lambda x: x[1])
+    best_segment = maximum[0]
+    return best_segment
+
+def get_set_score(letter_set, letter_list):
+    if not letter_set:
+        return 0
+    set_length = len(letter_set)
+    occurrence = 0
+    for letter in letter_set:
+        if letter in letter_list:
+            occurrence += 1
+
+    coverage = occurrence / len(letter_list)
+    score = coverage / set_length
+    return score
+
+def pick_best_set(scores1, scores2):
+    """
+    get the most 'useful' set
+    :param scores1:
+    :param scores2:
+    :return:
+    """
+    # best_counts = Counter()
+    # for score1, score2 in zip(scores1, scores2):
+    #     best_set_segment = max(score1, score2)
+    #     if best_set_segment is score1:
+    #         best_counts[0] += 1
+    #     else:
+    #         best_counts[1] += 1
+
+    best_set = max(scores1, scores2, key=lambda x: max(x))
+    if best_set is scores1:
+        return 0
+    else:
+        return 1
+
+
+def filter_spurious_data(unique_set):
+    for segment_point in unique_set:
+        threshold = min(5, sum(segment_point.values()) * 0.001)
+        good_segments = {}
+        for segment, count in segment_point.items():
+            if count > threshold:
+                good_segments[segment] = count
         yield good_segments
+
+
+
+def run_letters(first, second):
+    first_list, second_list = list(find_letters(first)), list(find_letters(second))
+    differences_1, differences_2 = get_differences(first_list, second_list)
+
+    scores_1 = [get_set_score(unique_set, letters_at_segment)
+                for unique_set, letters_at_segment in zip(differences_1, first_list)]
+    scores_2 = [get_set_score(unique_set, letters_at_segment)
+                for unique_set, letters_at_segment in zip(differences_2, second_list)]
+
+    best_set = pick_best_set(scores_1, scores_2)
+
+    print('best set', best_set)
+
+    # run it again with filtered data
+    if best_set == 0:
+        second_list = filter_spurious_data(second_list)
+        new_differences1, _ = get_differences(first_list, second_list)
+        differences = new_differences1
+        best_segment = pick_best_segment(scores_1)
+    else:
+        first_list = filter_spurious_data(first_list)
+        _, new_differences2 = get_differences(first_list, second_list)
+        differences = new_differences2
+        best_segment = pick_best_segment(scores_2)
+
+
+    differences = set(chain.from_iterable(subset for subset in differences if subset))
+    return differences, best_segment
+
+
 
 
 def run(file):
     with open(file) as words_file:
       words = json.load(words_file)
 
+
+    # left to right
+    print('left to right')
     first, second = words
+    # first = ngramicise(first)
+    # second = ngramicise(second)
+    ltr = run_letters(first, second)
+    pprint(ltr)
 
-    first_list, second_list = list(find_letters(first)), list(find_letters(second))
-    # filtered_first, filtered_second = filter_noise(first_list), filter_noise(second_list)
-    differences = get_differences(first_list, second_list)
+    # right to left
+    print()
+    print('*' * 10)
+    print('right to left')
+    first, second = words
+    first = [list(reversed(word)) for word in first]
+    second = [list(reversed(word)) for word in second]
+    # first = ngramicise(first)
+    # second = ngramicise(second)
+    rtl = run_letters(first, second)
+    pprint(rtl)
 
-    unique_letters = set()
-    which_group = Counter()
-    for difference, group in differences:
-        unique_letters.update(difference)
-        which_group[group] += 1
 
-    print('intermediate result')
-    pprint(unique_letters)
 
-    better_group = max(which_group, key=lambda item: which_group[item])
 
-    if better_group == 1:
-        first_list = filter_noise(first_list)
-    else:
-        second_list = filter_noise(second_list)
-
-    # run it again
-    differences = get_differences(first_list, second_list)
-    print(set(letters.pop() for letters, _ in differences))
 
 
 
